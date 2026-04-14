8c1c384946e3c8476d4eabe90c50618facb8620f
Admin <admin@local>
2026-04-14 13:25:16 +0200

    Add SVG icon as React component: IconFacebook

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,4 +2,5 @@ export { IconChat } from './IconChat';
 export { IconClock } from './IconClock';
 export { IconError } from './IconError';
 export { IconEye } from './IconEye';
+export { IconFacebook } from './IconFacebook';
 export type { IconProps } from './IconChat';
