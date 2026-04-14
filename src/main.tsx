3679ec81600171f9f028f2ffe90a5453d97b4f73
Admin <admin@local>
2026-04-14 12:19:11 +0200

    Add Sass and move typography tokens to SCSS

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 
 // Ant Design base styles
 import 'antd/dist/reset.css';
-import './styles/typography.css';
+import './styles/typography.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
